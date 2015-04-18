bd4b2a495cac2443807fe153a59068729d952da8
Vojtech Krasa <vojta.krasa@gmail.com>
2015-04-18 19:02:18 +0200

    queue limit - fix for new sessions

--- src/main/java/net/voldrich/smscsim/spring/auto/DelayedRequestSenderManager.java
+++ src/main/java/net/voldrich/smscsim/spring/auto/DelayedRequestSenderManager.java
@@ -18,14 +18,15 @@ public class DelayedRequestSenderManager {
 	private SmppSessionManager sessionManager;
 	private int queuesLimit = 100_000;
 
-	protected Thread monitorThread;
 	private Set<DelayedRequestSender> delayedRequestSenders = Collections.newSetFromMap(
 			new ConcurrentWeakKeyHashMap<DelayedRequestSender, Boolean>());
+	protected QueueMonitor queueMonitor;
 
 	public DelayedRequestSenderImpl getNewDeliverSender(String id) {
 		DelayedRequestSenderImpl delayedRequestSender = new DelayedRequestSenderImpl(sessionManager);
 		delayedRequestSender.start(id);
 		delayedRequestSenders.add(delayedRequestSender);
+		queueMonitor.applyStatus(delayedRequestSender);
 		return delayedRequestSender;
 	}
 
@@ -38,17 +39,11 @@ public class DelayedRequestSenderManager {
 		} catch (Exception e) {
 			log.warn("using queueLimit=" + queuesLimit, e);
 		}
-		monitorThread = new Thread(new QueueMonitor(delayedRequestSenders, queuesLimit),
+		queueMonitor = new QueueMonitor(delayedRequestSenders, queuesLimit);
+		Thread monitorThread = new Thread(queueMonitor,
 				"DelayedRequestSenderManagerMonitor");
 		monitorThread.setDaemon(true);
 		monitorThread.start();
-	}
-
-	@PreDestroy
-	public void stop() {
-		if (monitorThread != null) {
-			monitorThread.interrupt();
-		}
 	}
 
 	private static class QueueMonitor implements Runnable {
@@ -96,5 +91,8 @@ public class DelayedRequestSenderManager {
 			}
 		}
 
+		public void applyStatus(DelayedRequestSenderImpl delayedRequestSender) {
+			delayedRequestSender.setDiscardNewDeliveries(queueBlocked);
+		}
 	}
 }
